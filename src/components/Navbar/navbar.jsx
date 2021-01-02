a93eeb4604543ca932596cc3c60f1e4db8375543
knottyChive <jalengilyard99@gmail.com>
2021-01-02 20:27:46 +0000

    Starter Kit for Newsify from Si

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Navbar as Bar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <li>Nav</li>
-            <li>News</li>
-            <li>About</li>
-        </nav>
+        <Bar bg="primary" expand="lg">
+            <Bar.Brand as={Link} to="/">Newsify</Bar.Brand>
+            <Bar.Toggle aria-controls="mainNav" />
+            <Bar.Collapse id="mainNav">
+                <Nav className="ml-auto">
+                    <Nav.Link as={Link} to="/cnn">
+                        CNN
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/buzzfeed">
+                        Buzzfeed
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/all">
+                        All
+                    </Nav.Link>
+                </Nav>
+            </Bar.Collapse>
+        </Bar>
     )
 }
 
